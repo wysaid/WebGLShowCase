b7489538b2fac3d76d7892612b6c8f9a13bdbd00
wysaidAtCompany <wysaid@gmail.com>
2014-03-27 15:36:17 +0800

    完善框架代码

--- functions.js
+++ functions.js
@@ -4,3 +4,35 @@ Date: 2014-3-26
 Blog: http://blog.wysaid.org/
 */
 
+function bodyResize()
+{
+	var wrapperObj = document.getElementById("showcase-wrapper");
+	var canvasObj = document.getElementById("showcase");
+	canvasObj.width = wrapperObj.clientWidth;
+	canvasObj.height = wrapperObj.clientHeight;
+	wrapperObj.title = "viewport: " + wrapperObj.clientWidth + " x " + wrapperObj.clientHeight;
+}
+
+function runCode()
+{
+	var codeWrapper = document.getElementById("code-wrapper");
+	var codeArea = document.getElementById("codearea");
+	var htmlArea = document.getElementById("htmlarea");
+
+	codeWrapper.innerHTML = htmlarea.innerHTML;
+	var scriptObj = document.createElement("script");
+	scriptObj.innerHTML = codeArea.innerHTML;
+	scriptObj.setAttribute("type", "text/javascript");
+	codeWrapper.appendChild(scriptObj);
+}
+
+function runCodeInNewWindow()
+{
+
+}
+
+function maximizeCodeArea()
+{
+
+}
+
